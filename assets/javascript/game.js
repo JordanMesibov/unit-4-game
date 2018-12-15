cf782f3fd187b6ec2154ba3c63f226d45d734631
shinyscyther <mesibovj@gmail.com>
2018-12-15 14:56:47 -0500

    pseudocoded the loss conditions of the javascript file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,10 +13,14 @@
 
 // make it so that after the user gets alerted that they won a round, their win counter goes up by 1 and it gets displayed in the appropriate div.
 
-// make it so that once the user gets their win alert and the win counter goes up by 1, a a new round starts. A new round starts when the user's round score is reset to 0, a new round target score is generated and displayed, and new point values are assigned to the 4 crystal images/buttons.
+// make it so that once the user gets their win alert and the win counter goes up by 1, and a new round starts. A new round starts when the user's round score is reset to 0, a new round target score is generated and displayed to the appropriate div, and new point values are assigned to the 4 crystal images/buttons.
 
 
 
 // JS CODE FOR USER LOSING THE ROUND: ----------------------------
 
-// 
+// make it so that if the user's round score is greater than the target round score, the user gets alerted that they lost the round!
+
+//make it so that after the user gets alerted that they lost a round, their loss counter goes up by 1 and it gets displayed in the appropriate div.
+
+// make it so that once the user gets their loss alert and the loss counter goes up by 1, and a new round starts. A new round starts when the user's round score is reset to 0, a new round target score is generated and displayed to the appropriate div, and the new point values are assigned to the 4 crystal images/buttons.
