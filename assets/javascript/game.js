a308434f09a4e46227c6c6b0493ba280615418cf
shinyscyther <mesibovj@gmail.com>
2018-12-15 14:49:07 -0500

    pseudocoded the win conditions for the javascript file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,4 +2,21 @@
 
 // then, somewhat random point values must be assigned to each of the 4 crystal images/buttons
 
-// make it so that when the user clicks on any a crystal button, they increase their score for the round
+// make it so that when the user clicks on any a crystal button, they increase their score for the round and this is displayed in the appropriate div as text
+
+
+
+
+// JS CODE FOR USER WINNING THE ROUND: ---------------------
+
+// make it so that if the user's round score is exactly equal to the target round score, they get alerted that they won the round!
+
+// make it so that after the user gets alerted that they won a round, their win counter goes up by 1 and it gets displayed in the appropriate div.
+
+// make it so that once the user gets their win alert and the win counter goes up by 1, a a new round starts. A new round starts when the user's round score is reset to 0, a new round target score is generated and displayed, and new point values are assigned to the 4 crystal images/buttons.
+
+
+
+// JS CODE FOR USER LOSING THE ROUND: ----------------------------
+
+// 
