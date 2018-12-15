546f0edda68e27b02b09063ef101d3dac1f06970
shinyscyther <mesibovj@gmail.com>
2018-12-15 14:34:03 -0500

    added the jQuery cdn src link within the final script tags of the body of the html file and began pseudocoding the javascript file

---
+++ assets/javascript/game.js
@@ -0,0 +1,5 @@
+// first, the computer will have to randomly choose a target score number for the round
+
+// then, somewhat random point values must be assigned to each of the 4 crystal images/buttons
+
+// make it so that when the user clicks on any a crystal button, they increase their score for the round
